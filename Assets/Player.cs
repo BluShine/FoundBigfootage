3aaa73a1988633912c12f740f4124e48d938d567
BluShine <umbreon27@gmail.com>
2015-08-22 11:13:48 -0400

    Added walking controls and improved animation.
    Added controls for walking and improved the walking animation for
    bigfoot.

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if (horiz < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (horiz > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        transform.position += new Vector3(speed * Time.deltaTime * horiz,
+            speed * Time.deltaTime * vert, 0);
+	}
+}
