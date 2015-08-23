7b35b955f25b1add0527053273f5d4b18bf80219
BluShine <umbreon27@gmail.com>
2015-08-23 03:03:56 -0400

    Added camera dots for enemy raycasts.
    Added dots to indicate what the enemy's vision is being blocked by.

--- Assets/VideoDude.cs
+++ Assets/VideoDude.cs
@@ -4,30 +4,62 @@ using System.Collections;
 public class VideoDude : MonoBehaviour {
 
     public float speed = 5;
+    public VideoScore videoScore;
+    public LayerMask raycastMask;
+    public GameObject body;
     Player player;
     Animator anim;
+    public GameObject camDotPrefab;
+    GameObject[] camDots;
+    static int MAXDOTS = 10;
 
 	// Use this for initialization
 	void Start () {
-        anim = GetComponent<Animator>();
+        anim = body.GetComponent<Animator>();
         player = FindObjectOfType<Player>();
+        //go ahead and make some dots ahead of time.
+        camDots = new GameObject[MAXDOTS];
+        for (int i = 0; i < MAXDOTS; i++)
+        {
+            camDots[i] = GameObject.Instantiate(camDotPrefab);
+            camDots[i].SetActive(false);
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate() {
+        //MOVEMENT------------------------------------------------------
         Vector3 dir = player.transform.position - transform.position;
         dir.Normalize();
         transform.position += dir * Time.deltaTime * speed;
 
         if (dir.x > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            body.transform.localScale = new Vector3(1, 1, 1);
         }
         if (dir.x < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            body.transform.localScale = new Vector3(-1, 1, 1);
         }
 
         anim.speed = speed / 2;
+
+        //FILMING------------------------------------------------------
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position,
+            player.transform.position - transform.position, 
+            Vector2.Distance(transform.position, player.transform.position),
+            raycastMask);
+        videoScore.SetScore(hits.Length);
+        for (int i = 0; i < MAXDOTS; i++)
+        {
+            if(i < hits.Length){
+                camDots[i].SetActive(true);
+                camDots[i].transform.position = hits[i].point;
+            }
+            else
+            {
+                camDots[i].SetActive(false);
+            }
+        }
 	}
 }
--- Assets/VideoScore.cs
+++ Assets/VideoScore.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class VideoScore : MonoBehaviour {
 
+    public TextMesh multiplier;
+    public TextMesh qualityName;
+
+    static string[] QNAMES = {"GREAT", "GOOD", "DECENT", "AVERAGE", "NOT GREAT", 
+                                 "MEDIOCRE", "POOR", "BAD", "HORRIBLE", "GARBAGE", 
+                                 "POTATO"};
+    static float QINTERVAL = .2f;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void SetScore(float s)
+    {
+        multiplier.text = "X" + s;
+        if (s < QNAMES.Length)
+        {
+            qualityName.text = QNAMES[Mathf.FloorToInt(s)];
+        }
+        else
+        {
+            qualityName.text = QNAMES[QNAMES.Length - 1];
+        }
+    }
 }
