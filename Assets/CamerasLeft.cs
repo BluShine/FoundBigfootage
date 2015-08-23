9de83cfb8167820fbb9d238972795b8108a1a326
BluShine <umbreon27@gmail.com>
2015-08-23 09:46:17 -0400

    Added UI.
    Added UI to display score, remaining cameras, and episode title.

---
+++ Assets/CamerasLeft.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CamerasLeft : MonoBehaviour {
+
+    Text text;
+    public int camerasLeft = 0;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        camerasLeft = GameObject.FindGameObjectsWithTag("VideoEnemy").Length;
+        text.text = camerasLeft + " cameras remaining";
+	}
+}
---
+++ Assets/TotalScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TotalScore : MonoBehaviour {
+
+    Text text;
+    public float score = 0;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Score: " + Mathf.FloorToInt(score);
+	}
+}
