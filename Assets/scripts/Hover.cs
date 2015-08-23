da60b415a02361d21dd8daf5a4e38914d4695593
BluShine <umbreon27@gmail.com>
2015-08-23 18:02:13 -0400

    Made title screen functional.
    Added a script so that the title screen lets you start the game. Also
    added a tiny script to make "press enter" hover up and down.

---
+++ Assets/scripts/Hover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+
+    public float hoverAmount = 10f;
+    public float hoverSpeed = 1;
+    float hovery = 0;
+    Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hovery += Time.deltaTime * hoverSpeed;
+        hovery %= Mathf.PI * 2;
+        transform.position = startPos + new Vector3(0, Mathf.Sin(hovery), 0);
+	}
+}
---
+++ Assets/scripts/Titles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Titles : MonoBehaviour {
+
+    public GameObject[] showObjects;
+    public float showtime = 2f;
+    public string nextScene;
+
+    float timer = 0;
+    int count = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Submit") != 0)
+        {
+            Application.LoadLevel(nextScene);
+        }
+
+        timer += Time.deltaTime;
+        if (timer >= showtime)
+        {
+            timer = 0;
+            if (count < showObjects.Length)
+            {
+                showObjects[count].SetActive(true);
+            }
+            count++;
+        }
+	}
+}
